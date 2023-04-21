db08393c77290942769e2ed15def790eb82dc840
maciek367 <maciek2baba2@gmail.com>
2023-04-21 14:23:33 +0200

    add dependencies, change about and home page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page</p>
-      <Link href="/">Link to Home Page</Link>
+    <div className="hero flex flex-col gap-2">
+      <h1>This is page about our company</h1>
+      <p>Desci</p>
+      <Link className="link-hover" href="/">Link to Home Page</Link>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
+import { IoCarSport } from "react-icons/io5";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>This is the home page</p>
-      <Link href="/about">Link to About Page</Link>
+    <div className="flex justify-center items-center flex-col gap-2">
+      <div className="flex w-full">
+        <p className="flex text-green-500 gap-2 text-4xl items-center justify-start font-bold uppercase">
+          <IoCarSport />
+          <span>Cars</span>
+        </p>
+        <nav className="">
+          <Link href="/about">About</Link>
+          <Link href="/about">About</Link>
+          <Link href="/about">About</Link>
+        </nav>
+      </div>
+      <p>This is the home page of our company</p>
+      <Link className="link-hover" href="/about">
+        Link to About Page
+      </Link>
     </div>
   );
 }
