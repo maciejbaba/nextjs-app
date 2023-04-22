8e4f3654424d4add6bbd73775ba7516bada11e27
maciek367 <maciek2baba2@gmail.com>
2023-04-22 21:14:57 +0200

    add links, change nav

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center flex-col gap-2">
-      <div className="flex w-full">
-        <p className="flex text-green-500 gap-2 text-4xl items-center justify-start font-bold uppercase">
+    <div className="p-2 flex justify-center items-center flex-col gap-2">
+      <div className="flex w-full justify-between">
+        <p className="flex text-green-500 gap-2 text-4xl items-center font-bold uppercase">
           <IoCarSport />
           <span>Cars</span>
         </p>
-        <nav className="">
-          <Link href="/about">About</Link>
-          <Link href="/about">About</Link>
-          <Link href="/about">About</Link>
+        <nav className="flex gap-4 items-center">
+          <Link className="link-hover" href="/cars">List of cars</Link>
+          <Link className="link-hover" href="/about">About</Link>
+          <Link className="link-hover" href="/privacy">Privacy</Link>
         </nav>
       </div>
       <p>This is the home page of our company</p>
