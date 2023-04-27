95fd3a5506c127a186c19734d01720f82128dd54
maciek367 <maciek2baba2@gmail.com>
2023-04-27 23:26:30 +0200

    add link to cars list on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { IoCarSport } from "react-icons/io5";
 import Link from "next/link";
 
 export default function Home() {
+
   return (
     <div className="p-2">
-      <div className="flex flex-col gap-5 m-auto items-center w-1/2 text-center">
+      <section className="flex flex-col gap-5 m-auto items-center w-1/2 text-center">
         <h1 className="text-xl font-bold">
           Welcome to our website dedicated to all things cars!{" "}
         </h1>
@@ -13,7 +13,10 @@ export default function Home() {
           someone who appreciates the beauty and power of automobiles,
           you&apos;ve come to the right place.
         </p>
-      </div>
+        <Link href="/cars" className="px-4 py-2 rounded bg-blue-500 font-bold hover:bg-blue-700">
+          See our cars
+        </Link>
+      </section>
     </div>
   );
 }
