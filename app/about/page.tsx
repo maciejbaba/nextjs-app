31da35832931c5f4aacf0bcd2e07cd4ef422ee68
maciek367 <maciek2baba2@gmail.com>
2023-04-27 22:44:42 +0200

    change page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,34 @@
-
 export default function About() {
   return (
-    <div className="flex flex-col text-center items-center gap-2">
-      <section>
-			<h2 className="text-2xl">Our Mission</h2>
-			<p>At our company, we are dedicated to providing high-quality vehicles and exceptional customer service to our valued clients. Our goal is to make the car buying process as seamless and stress-free as possible.</p>
-		</section>
+    <div className="flex flex-col text-center items-center gap-10">
+      <section className="w-1/2">
+        <h2 className="text-2xl">Our Mission</h2>
+        <p>
+          At our company, we are dedicated to providing high-quality vehicles
+          and exceptional customer service to our valued clients. Our goal is to
+          make the car buying process as seamless and stress-free as possible.
+        </p>
+      </section>
 
-		<section>
-			<h2 className="text-2xl">Our Cars</h2>
-			<p>We offer an extensive selection of cars from top manufacturers, including Next and Tailwind. Whether you&apos;re looking for a sports car, SUV, or family vehicle, we have a wide range of options to meet your needs.</p>
-		</section>
+      <section className="w-1/2">
+        <h2 className="text-2xl">Our Cars</h2>
+        <p>
+          We offer an extensive selection of cars from top manufacturers,
+          including Next and Tailwind. Whether you&apos;re looking for a sports
+          car, SUV, or family vehicle, we have a wide range of options to meet
+          your needs.
+        </p>
+      </section>
 
-		<section>
-			<h2 className="text-2xl">Our Team</h2>
-			<p>Our team of experienced professionals is dedicated to helping you find the perfect vehicle to meet your needs. We offer a range of financing options to fit your budget and credit score, and our knowledgeable staff is always available to answer any questions you may have.</p>
-		</section>
+      <section className="w-1/2">
+        <h2 className="text-2xl">Our Team</h2>
+        <p>
+          Our team of experienced professionals is dedicated to helping you find
+          the perfect vehicle to meet your needs. We offer a range of financing
+          options to fit your budget and credit score, and our knowledgeable
+          staff is always available to answer any questions you may have.
+        </p>
+      </section>
     </div>
   );
 }
