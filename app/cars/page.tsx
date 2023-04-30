c474fb9eebe14f45d297d06654684a045bb254b3
maciek367 <maciek2baba2@gmail.com>
2023-04-30 15:07:41 +0200

    add basic layout

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -1,5 +1,13 @@
 export default function Cars() {
   return (
-    <div>cars</div>
+    <main className="flex flex-col justify-center items-center">
+      <h1 className="text-2xl">Cars</h1>
+      <section className="">
+        
+        <h2>Car 1</h2>
+        <p>Car 1 description</p>
+
+      </section>
+    </main>
   )
 }
